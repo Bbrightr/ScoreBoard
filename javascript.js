617737445fd38150e3c278ce0642623515305e18
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2024-03-23 20:26:58 +0100

    Scoreboard
    they say practice makes mastery, that's why I'm practicing my JavaScript by attempting solo projects like this scoreboard

---
+++ javascript.js
@@ -0,0 +1,42 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let hScore = 0
+let gScore = 0
+
+//Home Scores
+function add1PointH(){
+    hScore += 1
+    console.log(hScore)
+    homeScore.textContent = hScore
+}
+
+function add2PointH(){
+    hScore += 2
+    console.log(hScore)
+    homeScore.textContent = hScore
+}
+
+function add3PointH(){
+    hScore += 3
+    console.log(hScore)
+    homeScore.textContent = hScore
+}
+
+//Guest Scores
+function add1PointG(){
+    gScore += 1
+    console.log(gScore)
+    guestScore.textContent = gScore
+}
+
+function add2PointG(){
+    gScore += 2
+    console.log(gScore)
+    guestScore.textContent = gScore
+}
+
+function add3PointG(){
+    gScore += 3
+    console.log(gScore)
+    guestScore.textContent = gScore
+}
