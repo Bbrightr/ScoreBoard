38cb2cd2eef3fe0655ff7c816de1ed5ad9a7105b
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-02-17 22:41:48 +0100

    Timer Added
    Timer added on scoreboard

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let guestScore = document.getElementById("guest-score")
 let hScore = 0
 let gScore = 0
 
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime()
+
 //Home Scores
 function add1PointH(){
     hScore += 1
@@ -41,7 +43,36 @@ function add3PointG(){
     guestScore.textContent = gScore
 }
 
-function clearBoard(){
+
+
+//timer
+
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 5 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+    document.getElementById("timer").innerHTML = hours + " : " + minutes + " : " + seconds ;
+      
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  function clearBoard(){
     guestScore.textContent = 0
     homeScore.textContent = 0
+    document.getElementById("timer").innerText = "00:00:00"
 }
