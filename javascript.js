ab1f18598a5d6181ffa0aaf323b2406fe414e2f9
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-02-18 21:19:21 +0100

    updating for netlify

--- javascript.js
+++ javascript.js
@@ -43,25 +43,28 @@ function add3PointG(){
     guestScore.textContent = gScore
 }
 
-
+function clearBoard(){
+    gScore = 0
+    guestScore.innerHTML = 0
+    // console.log()
+    hScore = 0
+    homeScore.textContent = 0
+    
+    let timerEl = document.getElementById("stopTimer")
+    timerEl.textContent = "00:00:00"
+}
 
 //timer
 
 
 
 
-  function clearBoard(){
-    guestScore.textContent = 0
-    homeScore.textContent = 0
-    document.getElementById("timer").innerText = "00:00:00"
-}
 
-// function startNewGame() {
-    
-//     var x = setInterval(function() {
 
-//         // Get today's date and time
-//         var now = new Date().getTime();
+// var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+//     var x = setInterval(function()
+//     { var now = new Date().getTime();
           
 //         // Find the distance between now and the count down date
 //         var distance = countDownDate - now;
@@ -72,36 +75,41 @@ function add3PointG(){
 //         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 //         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
           
-//         // Output the result in an element with id="demo"
-//         document.getElementById("timer").innerHTML = hours + " : " + minutes + " : " + seconds ;
-          
-//         // If the count down is over, write some text 
-//         if (distance < 0) {
-//           clearInterval(x);
-//           document.getElementById("timer").innerHTML = "00:00:00";
-//         }
+//         // Output the result in an element with id="stopTimer"
+//         document.getElementById("stopTimer").innerHTML = hours + " : " + minutes + " : " + seconds ;
+              
+//             // If the count down is over, write some text 
+//             if (distance < 0) {
+//               clearInterval(x);
+//               document.getElementById("stopTimer").innerHTML = "00:00:00";
+//             }
 //       }, 1000);
+    
+// function startNewGame() {
+//         document.getElementById("stopTimer").innerHTML = hours + " : " + minutes + " : " + seconds ;
 // }
+
 
 // function startNewGame(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-//         display.textContent = minutes + ":" + seconds;
+        display.textContent = minutes + ":" + seconds;
 
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 // }
 
-// document.getElementById("timer") = function () {
-//     var fiveMinutes = 60 * 5,
-//         display = document.querySelector("timer");
-//     startTimer(fiveMinutes, display);
-// };
+document.getElementById("stopTimer") = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector("stopTimer");
+    startTimer(fiveMinutes, display);
+};
+
